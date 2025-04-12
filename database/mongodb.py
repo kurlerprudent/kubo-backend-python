503b990474a7c63ae98dad1ac7a742459f6092ef
gene-999 <eugenebaidoo5@gmail.com>
2025-04-12 15:34:53 +0000

    setup the db

--- database/mongodb.py
+++ database/mongodb.py
@@ -4,6 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+
+from pymongo import MongoClient
+
+# You can change this when you use a cloud hosted version of the database
+client = MongoClient('localhost',27017)
+
+# MONGO_URI = os.getenv("MONGO_URI") or 
+# client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+
 db = client["hospital_system"]
+
